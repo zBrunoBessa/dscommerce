76e467c388f0fefcfefab05669cd29784e34079e
Bruno Bessa <beduardo2014@gmail.com>
2023-11-21 16:59:49 -0300

    consultas JPQL

--- src/main/java/com/brunobessa/dscommerce/repositories/ProductRepository.java
+++ src/main/java/com/brunobessa/dscommerce/repositories/ProductRepository.java
@@ -1,7 +1,16 @@
 package com.brunobessa.dscommerce.repositories;
 
 import com.brunobessa.dscommerce.entities.Product;
+import jakarta.validation.constraints.Size;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    @Query("SELECT obj FROM Product obj " +
+            "WHERE UPPER(obj.name) LIKE UPPER(CONCAT('%', :name, '%'))")
+    Page<Product> searchByName(String name, Pageable pageable);
 }
