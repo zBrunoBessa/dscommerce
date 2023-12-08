84770dc222a68f502972213ab19a17f968c54c97
Bruno Bessa <beduardo2014@gmail.com>
2023-12-08 10:53:38 -0300

    UserDetails,  GrantedAuthority, not tested

--- src/main/java/com/brunobessa/dscommerce/entities/User.java
+++ src/main/java/com/brunobessa/dscommerce/entities/User.java
@@ -1,13 +1,15 @@
 package com.brunobessa.dscommerce.entities;
 
 import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDate;
 import java.util.*;
 
 @Entity
 @Table(name = "tb_user")
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -109,6 +111,37 @@ public class User {
             }
         }
         return false;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles;
+    }
+
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 
     @Override
